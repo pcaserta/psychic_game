eb2e29e2ae30ae8bf2fe64d581b8c41959a4dfc6
pcaserta <patrick.caserta@live.longwood.edu>
2019-02-26 18:18:27 -0500

    ay

--- java.js
+++ java.js
@@ -1,49 +1,63 @@
-// Variable for the computers choices //
-var computerchoice = ['a','b','c','d','e','f','g','h','i','j','k','l',
-    'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+ // Variable for the computers choices //
+ 
+ var computerchoice = ['a','b','c','d','e','f','g','h','i','j','k','l',
+ 'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-    // variables for wins, losses, guesses left//
+ // variables for wins, losses, guesses left//
 var wins = 0;
 var losses = 0;
-var guesses_left = 10;
+var guesses_left = 9;
+var yourguesses = []
 
 // Create variables that hold references to the places in the HTML where we want to display things.
 
 var winsText = document.getElementById("Wins-text");
 var lossestext = document.getElementById("losses-text");
-var guessestext = document.getElementById("user-guesses");
+var guesseslefttext = document.getElementById("guesses-left");
+var yourguessestext = document.getElementById("user-guesses")
 
-//function is run when user presses key//
+//computer guess//
 
+var computerguess = computerchoice[Math.floor(Math.random() * computerchoice.length)];
+console.log(computerguess)
+
+
+//function is run  when user presses key//
 document.onkeyup = function(event) {
 
-    //determines which key was pressed//
+
+ //determines which key was pressed//
 
 var userGuess = event.key;
+console.log(userGuess)
 
-//randomly chooses a letter of the alphabet from array//
 
-var computerguess = computerchoice[Math.floor](math.random() * computerchoice.length)];
 
 //this if statment determines what happens if we guess the computer choice or not//
-                                {
-if (userGuess = computerguess) {
+                             { 
+if(userGuess === computerguess){
     wins++ 
-    guesses_left--
-  
+    guesses_left = 9
+    yourguesses = []
+    
+}
+if (guesses_left === 0) {
+losses++
+guesses_left = 9
+yourguesses = []
+
+
 } else {
-    losses++
+ guesses_left--
+ yourguesses.push(userGuess)
 }
 
 //display wins, losses, guesses, and guesses left//
 
 winsText.textContent = "Wins: " + wins;
 lossestext.textContent = "Losses: " + losses;
-guessestext.textContent = "Guesses left: " + guesses_left
+guesseslefttext.textContent = "Guesses left: " + guesses_left;
+yourguessestext.textContent = "Your Guesses so far: " + yourguesses;
 }
 
 }
-
-
-
-    
